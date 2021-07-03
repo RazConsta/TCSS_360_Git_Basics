38037941461acbd31c2d775150329dac5d505a7e
Dustin Ray <dustiv2@uw.edu>
2021-07-03 00:20:46 -0700

    Testing commit of files to repo

--- E521.java
+++ E521.java
@@ -1,41 +1,28 @@
-package tools.Elliptic_Curve;
+package Curve;
 import java.math.BigInteger;
 
 /**
- * E521 Elliptic Curve (Edward's Curve) of equation: (x^2) + (y^2) = 1 + d(x^2)(y^2)
+ * Curve.E521 Elliptic Curve (Edward's Curve) of equation: (x^2) + (y^2) = 1 + d(x^2)(y^2)
  * where d = -376014
- * Contains methods to add (and subtract), multiply, and "divide" points on curve using scalar values.
+ * Contains methods to add and multiply points on curve using scalar values.
  *
  * @version Spring 2021
  * @author Dustin Ray
  */
 public class E521 {
 
-    /**
-     * Mersenne prime defining a finite field F(p)
-     */
+    /** Mersenne prime defining a finite field F(p) */
     public static final BigInteger P = (new BigInteger("2").pow(521)).subtract(BigInteger.ONE);
-
-    /**
-     * d = -376014
-     */
+    /** d = -376014 */
     public static final BigInteger D = new BigInteger("-376014");
-
-
-    /**
-     * X-coordinate of point.
-     */
+    /** X-coordinate of point. */
     private BigInteger myX;
-
-    /**
-     * Y-coordinate of point.
-     */
-    private final BigInteger myY;
-
-    /** number of points on E521 Curve -> n := 4 * (R) .*/
+    /** Y-coordinate of point. */
+    private BigInteger myY;
+    /** number of points on Curve.E521 Curve -> n := 4 * (R) .*/
     public static final BigInteger R = (new BigInteger("2").pow(519))
             .subtract(new BigInteger(
-            "337554763258501705789107630418782636071904961214051226618635150085779108655765"));
+                    "337554763258501705789107630418782636071904961214051226618635150085779108655765"));
 
     /**
      * Constructor for point with only X provided. Solves for Y.
@@ -43,17 +30,13 @@ public class E521 {
      * from X using sqrt method.
      */
     public E521(final BigInteger theX, boolean theLSB) {
-
         myX = theX;
-
         //solve for y
         BigInteger num = (BigInteger.ONE.subtract(theX.pow(2))).mod(P);
         BigInteger denom = BigInteger.ONE.add(new BigInteger("376014").multiply(theX.pow(2))).mod(P);
         denom = denom.modInverse(P);
-
         BigInteger radicand = num.multiply(denom);
         myY = sqrt(radicand, P, theLSB);
-
     }
 
     /**
@@ -62,26 +45,21 @@ public class E521 {
      * @param theY y coordinate of point
      */
     public E521(final BigInteger theX, final BigInteger theY) {
-
         myX = theX;
         myY = theY;
-
     }
 
-    /**
-     * Constructor for neutral element, defined to be point (0, 1)
-     */
+    /** Constructor for neutral element, defined to be point (0, 1) */
     public E521() {
         myX = BigInteger.ZERO;
         myY = BigInteger.ONE;
     }
 
-
     /**
      * Compares two points for equality by X and Y coordinates.
      * Utilizes .equals method of BigInteger class.
      *
-     * @param theOther E521 point to be compared against this object.
+     * @param theOther Curve.E521 point to be compared against this object.
      * @return boolean, true for equal, else otherwise.
      */
     public boolean equals(final E521 theOther) {
@@ -93,68 +71,57 @@ public class E521 {
      * Gets the opposite value of a point, defined as the following:
      * if P = (X, Y), opposite of P = (-X, Y).
      *
-     * @return E521 point which is opposite of a given point as defined above.
+     * @return Curve.E521 point which is opposite of a given point as defined above.
      */
     public E521 getOpposite() {
         return new E521(myX.negate(), this.getY());
     }
 
     /**
-     * Adds two E521 points and returns another (X, Y) point which is on the E521 curve defined
+     * Adds two Curve.E521 points and returns another (X, Y) point which is on the Curve.E521 curve defined
      * using parameters set by constructor. Add operation is defined as:
      * (x1, y1) + (x2, y2) = ((x1y2 + y1x2) / (1 + (d)x1x2y1y2)), ((y1y2 - x1x2) / (1 - (d)x1x2y1y2))
      * where "/" is defined to be multiplication by modular inverse.
      *
      * @param theOther e521 point to be added to current point.
-     * @return E521 point which is the result of the addition operation defined here. Will
+     * @return Curve.E521 point which is the result of the addition operation defined here. Will
      * fail with exception if point is not on curve defined in class fields.
      */
     public E521 add(final E521 theOther) {
 
-//        System.out.println("Add: \n" + theOther.toString());
-        //test for G + (-G), then return neutral point. -G := (x, p - y)
+        BigInteger x1 = this.getX();
+        BigInteger x2 = theOther.getX();
 
-            BigInteger x1 = this.getX();
-            BigInteger x2 = theOther.getX();
+        BigInteger y1 = this.getY();
+        BigInteger y2 = theOther.getY();
 
-            BigInteger y1 = this.getY();
-            BigInteger y2 = theOther.getY();
+        BigInteger xNum = ((x1.multiply(y2)).add(y1.multiply(x2))).mod(P);
+        BigInteger xDenom = (BigInteger.ONE.add(D.multiply(x1).multiply(x2).multiply(y1).multiply(y2))).mod(P);
 
-            BigInteger xNum = ((x1.multiply(y2)).add(y1.multiply(x2))).mod(P);
-            BigInteger xDenom = (BigInteger.ONE.add(D.multiply(x1).multiply(x2).multiply(y1).multiply(y2))).mod(P);
+        xDenom = xDenom.modInverse(P);
+        BigInteger newX = xNum.multiply(xDenom).mod(P);
 
-            xDenom = xDenom.modInverse(P);
-            BigInteger newX = xNum.multiply(xDenom).mod(P);
+        BigInteger yNum = (y1.multiply(y2).subtract(x1.multiply(x2))).mod(P);
+        BigInteger yDenom = (BigInteger.ONE.subtract(D.multiply(x1).multiply(x2).multiply(y1).multiply(y2))).mod(P);
 
-            BigInteger yNum = (y1.multiply(y2).subtract(x1.multiply(x2))).mod(P);
-            BigInteger yDenom = (BigInteger.ONE.subtract(D.multiply(x1).multiply(x2).multiply(y1).multiply(y2))).mod(P);
+        yDenom = yDenom.modInverse(P);
+        BigInteger newY = yNum.multiply(yDenom).mod(P);
 
-            yDenom = yDenom.modInverse(P);
-            BigInteger newY = yNum.multiply(yDenom).mod(P);
-
-            return new E521(newX, newY);
-
-
+        return new E521(newX, newY);
     }
 
-    /**
-     * Naive multiplication algorithm
-     * @param theS is the secret key to multiply by.
-     * @param P is the point to multiply.
-     * @return sP which is the product of s * P
-     */
     public E521 multiply(final BigInteger theS, final E521 P) {
-
         E521 result = new E521(BigInteger.ZERO, BigInteger.ONE);
         for(int i = 0; i < theS.intValue(); i ++) {
             result = result.add(P);
         }
         return result;
     }
+
     /**
-     * Double and add multiplication for E521 point, index decreasing.
+     * Double and add multiplication for Curve.E521 point, index decreasing.
      * @param theS Scalar value to multiply by
-     * @return E521 point which is product of multiplication
+     * @return Curve.E521 point which is product of multiplication
      * with S.
      */
     public E521 doubleAndAdd(final BigInteger theS) {
@@ -162,8 +129,6 @@ public class E521 {
         E521 result = new E521(BigInteger.ZERO, BigInteger.ONE);
         int idx = theS.bitLength();
         while (idx >= 0) {
-
-//            System.out.println(idx);
             result = result.add(result);
             if (theS.testBit(idx--)) {
                 result = result.add(this);
@@ -171,8 +136,6 @@ public class E521 {
         }
         return result;
     }
-
-
 
     /**
      * EC Multiplication algorithm using the Montgomery Ladder approach to mitigate
@@ -182,7 +145,7 @@ public class E521 {
      * be replaced with individual doubling algorithm.
      *
      * @param theS scalar value to multiply by. S is a private key and should be kept secret.
-     * @return E521 point which is result of multiplication.
+     * @return Curve.E521 point which is result of multiplication.
      */
     public E521 multiplyMontgomery(final BigInteger theS, final E521 P) {
 
@@ -190,7 +153,6 @@ public class E521 {
         E521 r1 = P;
         int idx = theS.bitLength();
         while (idx >= 0) {
-
             if (theS.testBit(idx--)) {
                 r0 = r0.add(r1);
                 r1 = r1.add(r1);
@@ -199,7 +161,7 @@ public class E521 {
                 r0 = r0.add(r0);
             }
         }
-        return r0; // r0 = this * s
+        return r0; // r0 = P * s
     }
 
 
@@ -214,13 +176,11 @@ public class E521 {
      * if such a root exists, otherwise null.
      */
     public static BigInteger sqrt(BigInteger v, BigInteger p, boolean lsb) {
-
         assert (p.testBit(0) && p.testBit(1)); // p = 3 (mod 4)
         if (v.signum() == 0) {
             return BigInteger.ZERO;
         }
         BigInteger r = v.modPow(p.shiftRight(2).add(BigInteger.ONE), p);
-//        System.out.println("r = " + r);
         if (r.testBit(0) != lsb) {
             r = p.subtract(r); // correct the lsb }
             return (r.multiply(r).subtract(v).mod(p).signum() == 0) ? r : null;
@@ -233,9 +193,9 @@ public class E521 {
      * Gets string representation of object as pair of X and Y coordinates.
      * @return String which is labelled pair of X and Y coordinates.
      */
-    public String toString() {
-        return "E521 Point: \n" + "X coordinate: " + getX().toString() + "\n"
-                + "Y coordinate: " + getY().toString();
+    public String toString(final String name) {
+        return name + "\n" + "X coordinate: " + getX().toString() + "\n"
+                + "Y coordinate: " + getY().toString() + "\n";
     }
 
     /**
@@ -246,7 +206,10 @@ public class E521 {
         return myX;
     }
 
+    /** Set X coordinate. */
     public void setX(final BigInteger theX) {myX = theX;}
+    /** Set Y coordinate. */
+    public void setY(final BigInteger theY) {myY = theY;}
 
     /**
      * Gets current Y value.
@@ -254,7 +217,9 @@ public class E521 {
      */
     public BigInteger getY() { return myY; }
 
-
+    /** Returns r value for curve.
+     * @return r value for curve. */
+    public BigInteger getR() { return R; }
 
 
 
